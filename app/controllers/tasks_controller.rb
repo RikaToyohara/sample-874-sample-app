804bab98c28ee79537e1d864939be0785e7d1562
RikaToyohara <rikachan814sun@gmail.com>
2020-02-24 01:10:47 +0000

    ヘッダーにタスク作成追加（リンクなし）

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_user
+  before_action :set_task, only: %i(show edit update destroy)
   
   def index
     @tasks = @user.tasks
@@ -9,10 +10,24 @@ class TasksController < ApplicationController
   end
   
   def new
+    @tasl = Task.new
   end
   
+  def create
+    @task = @user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = "タスクを新規作成しました。"
+      redirect_to user_tasks_url @user
+    else
+      render :new
+    end
+  end
   
   private
+  
+  def task_params
+      params.require(:task).permit(:name, :description)
+  end
   
   def set_user
       @user = User.find(params[:user_id])
