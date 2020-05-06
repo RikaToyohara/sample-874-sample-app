c995be48a8bb7acb3088a87a0767f5febf984dc0
RikaToyohara <rikachan814sun@gmail.com>
2020-05-06 06:14:11 +0000

    削除機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
-  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
   
   def  index
     @users = User.paginate(page: params[:page], per_page: 20) # １ページ２０名表示
@@ -39,7 +40,15 @@ class UsersController < ApplicationController
       render :edit
     end
   end  
-      
+ 
+ 
+  def destroy
+   @user.destroy
+   flash[:success] = "#{@user.name}のデータを削除しました。"
+   redirect_to users_url
+  end
+  
+  
   private
   
     def user_params
@@ -64,6 +73,11 @@ class UsersController < ApplicationController
     # アクセスしたユーザーが現在ログインしているユーザーかどうか確認
     def correct_user
       redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    # システム管理権限所有かどうか判定する
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end  
 end
 
