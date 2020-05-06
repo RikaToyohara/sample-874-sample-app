a6e6881630b04caf617bb66ef7dce3453cafd588
RikaToyohara <rikachan814sun@gmail.com>
2020-05-06 04:05:14 +0000

    indexアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   
+  def  index
+    @user = User.all
+  end  
+  
   def show
     @user = User.find(params[:id])
   end
@@ -59,8 +63,7 @@ class UsersController < ApplicationController
     
     # アクセスしたユーザーが現在ログインしているユーザーかどうか確認
     def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless @user == current_user
+      redirect_to(root_url) unless current_user?(@user)
     end  
 end
 
